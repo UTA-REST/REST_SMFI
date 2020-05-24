99aaa1199deb4f41020545957de31b0f5ed9e4fd
AustinMcDonald <austin.mcdonald96@mavs.uta.edu>
2020-05-24 12:52:43 -0500

    clean up

--- Core/Cython/build.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# build the code
-python3 setup_cython.py clean
-
-python3 setup_cython.py build_ext --inplace
-
-
--- Core/Cython/setup_cython.py
+++
@@ -1,9 +0,0 @@
-from distutils.core import setup
-from distutils.extension import Extension
-from Cython.Build import cythonize
-import  numpy
-print(numpy.get_include())
-ext_modules=[
-Extension("*",["*.pyx"],include_dirs=[numpy.get_include(),'.'])
-]
-setup(ext_modules=cythonize(ext_modules))
